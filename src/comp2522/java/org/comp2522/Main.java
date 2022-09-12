ca97ffa49db3518feadb348935c11cb125eb2ca0
bcherng <bcherng@my.bcit.ca>
2022-09-11 18:38:35 -0700

    Fixing checkstyle errors: Missing javadoc Indentations

--- src/comp2522/java/org/comp2522/Main.java
+++ src/comp2522/java/org/comp2522/Main.java
@@ -1,7 +1,10 @@
 package org.comp2522;
 
+/**
+Lab01 Main class: outputs information about the author.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Capybaras are really really neat.");
-    }
+  public static void main(String[] args) {
+    System.out.println("Capybaras are really really neat.");
+  }
 }
